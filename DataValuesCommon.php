025f510d2dcf2b72930045ba1dccb5fcec7f4082
jeroendedauw <jeroendedauw@gmail.com>
2013-09-20 21:30:06 +0200

    Use upper case for version constants
    Change-Id: I8a64135b265c67c0d89a8e66e0f4f3ea8a4adcb3

--- DataValuesCommon.php
+++ DataValuesCommon.php
@@ -5,32 +5,37 @@ if ( defined( 'DataValuesCommon_VERSION' ) ) {
 	return;
 }
 
-define( 'DataValuesCommon_VERSION', '0.1 alpha' );
+define( 'DATAVALUES_COMMON_VERSION', '0.1 alpha' );
+
+/**
+ * @deprecated
+ */
+define( 'DataValuesCommon_VERSION', DATAVALUES_COMMON_VERSION );
 
 // If one of the dependencies has not been loaded yet, attempt to include the Composer autoloader.
-if ( !defined( 'DataValues_VERSION' ) && is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+if ( !defined( 'DATAVALUES_VERSION' ) && is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	include_once( __DIR__ . '/vendor/autoload.php' );
 }
 
 // Attempt to include the DataValues lib if that hasn't been done yet.
 // This is the path the DataValues entry point will be at when loaded as MediaWiki extension.
-if ( !defined( 'DataValues_VERSION' ) && is_readable( __DIR__ . '/../DataValues/DataValues.php' ) ) {
+if ( !defined( 'DATAVALUES_VERSION' ) && is_readable( __DIR__ . '/../DataValues/DataValues.php' ) ) {
 	include_once( __DIR__ . '/../DataValues/DataValues.php' );
 }
 
 // Attempt to include the DataValuesInterfaces lib if that hasn't been done yet.
 // This is the path its entry point will be at when loaded as MediaWiki extension.
-if ( !defined( 'DataValuesInterfaces_VERSION' ) && is_readable( __DIR__ . '/../DataValuesInterfaces/DataValuesInterfaces.php' ) ) {
+if ( !defined( 'DATAVALUES_INTERFACES_VERSION' ) && is_readable( __DIR__ . '/../DataValuesInterfaces/DataValuesInterfaces.php' ) ) {
 	include_once( __DIR__ . '/../DataValuesInterfaces/DataValuesInterfaces.php' );
 }
 
 // Only initialize the extension when all dependencies are present.
-if ( !defined( 'DataValues_VERSION' ) ) {
+if ( !defined( 'DATAVALUES_VERSION' ) ) {
 	throw new Exception( 'You need to have the DataValues library loaded in order to use DataValuesCommon' );
 }
 
 // Only initialize the extension when all dependencies are present.
-if ( !defined( 'DataValuesInterfaces_VERSION' ) ) {
+if ( !defined( 'DATAVALUES_INTERFACES_VERSION' ) ) {
 	throw new Exception( 'You need to have the DataValuesInterfaces library loaded in order to use DataValuesCommon' );
 }
 
