86e58f9d63bb068d649136576f4c796c4999052f
jeroendedauw <jeroendedauw@gmail.com>
2013-11-16 03:11:35 +0100

    Return guard tweak

--- DataValuesCommon.php
+++ DataValuesCommon.php
@@ -1,8 +1,18 @@
 <?php
 
+/**
+ * Entry point of the DataValues Common library.
+ *
+ * @since 0.1
+ * @codeCoverageIgnore
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
 if ( defined( 'DataValuesCommon_VERSION' ) ) {
 	// Do not initialize more then once.
-	return;
+	return 1;
 }
 
 define( 'DATAVALUES_COMMON_VERSION', '0.1 beta' );
