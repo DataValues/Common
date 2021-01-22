9726088771c2107f605f18f659e455c27ef2e65b
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-01-22 15:29:14 +0100

    Add strict_types

--- src/ValueParsers/FloatParser.php
+++ src/ValueParsers/FloatParser.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace ValueParsers;
 
 use DataValues\NumberValue;
--- tests/ValueParsers/NullParserTest.php
+++ tests/ValueParsers/NullParserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace ValueParsers\Test;
 
 use DataValues\UnknownValue;
