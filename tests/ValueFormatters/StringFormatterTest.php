1f4b52edd988730a497c891ddc4db1b6843aa00d
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-01-22 15:29:14 +0100

    Add strict_types

--- tests/ValueFormatters/StringFormatterTest.php
+++ tests/ValueFormatters/StringFormatterTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace ValueFormatters\Test;
 
 use DataValues\StringValue;
--- tests/ValueParsers/Normalizers/TrimmingStringNormalizerTest.php
+++ tests/ValueParsers/Normalizers/TrimmingStringNormalizerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace ValueParsers\Normalizers\Test;
 
 use DataValues\StringValue;
